a5d531dc0b7f5cd9a292fcc89928bdef0958bce8
GoodDay360 <istartgame31@gmail.com>
2026-04-18 19:18:37 +0700

    fix: sort get_torrents

--- src/get_torrents.ts
+++ src/get_torrents.ts
@@ -17,8 +17,9 @@ export default async function get_torrents(input_payload: get_torrents_types.Inp
 
     let slug:String = data.data.movie.slug??"";
 
-    let torrent_list = data?.data?.movie?.torrents??[]
-        .filter((t:any) => parseInt(t.seeds??0) > 0);
+    let torrent_list = data?.data?.movie?.torrents??[];
+    
+    torrent_list = torrent_list.filter((t:any) => parseInt(t.seeds??0) > 0);
 
     torrent_list.sort((a:any, b:any) => parseInt(b.seeds??0) - parseInt(a.seeds??0));
 
